82c653b2ab08dff20c4ad92983a1e684f04eaed5
rasho22 <ab_rasho@yahoo.fr>
2016-07-11 17:15:49 +0200

    first commit

---
+++ questions.js
@@ -0,0 +1,25 @@
+
+
+function CreationTableauLangages(){
+	var langages=["Html","CSS","Java","PHP"];
+	return langages;
+
+};
+function CreationTableauNombres(){
+	var tab = [0,1,2,3,4,5];
+	return tab;
+};
+
+function RemplacementElement(langages){
+
+};
+
+function AjoutElementLangages (langages){
+	var tab2=["Ruby", "Python"];
+	return langages.concat(tab2);
+
+};
+function AjoutElementNombres(tab){
+	var tab3=[-2,-1];
+	return tab.unshift(tab3);
+};
